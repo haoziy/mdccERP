312132f8294adc22c75ee5ff89c08291f76bcb68
mirongjian@163.com <mirongjian@163.com>
2018-01-04 18:06:51 +0800

    新增统计按多种方式查询

--- web/src/Content/statistics/index.js
+++ web/src/Content/statistics/index.js
@@ -2,33 +2,70 @@
  * Created by shenqiao on 2017/12/23.
  */
 
-import {Table,Button,Modal,Input,Select,Alert,message} from 'antd'
+import {Table,Button,Modal,Input,Select,message} from 'antd'
 import React, {Component} from 'react'
 import {CustomerHandler} from '../../HttpRequest/CustomerHandler'
-import {checkPhone,valiedString} from '../../utils'
-import ExportJsonExcel from 'js-export-excel'
 import moment from 'moment'
 
 export  default class StatisticsViewController extends Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-
-        }
+        this.state = {}
     }
-
-
-
 
     render() {
         return (
             <div style={{margin:10}}>
-                <div style={{flexDirection:'row',justifyContent:'space-between',display:'flex'}}>
+                <div style={{flexDirection:'row',display:'flex',flex:1,borderBottomWidth:2,borderColor:'#ddd'}}>
                     <h3>成交详细</h3>
-                    <div>
-                        <Button type="primary" onClick={()=>this.setState({visible:true})} style={{marginRight:10}}>全部</Button>
+                </div>
+                <div style={{flexDirection:'row',display:'flex',alignItems:'center'}}>
+                    <Button type="primary" onClick={()=>this.setState({visible:true})}
+                            style={{marginRight:10}}>全部</Button>
+                    <div style={{margin:5}}>
+                        <h5>按员工统计</h5>
+                        <div>
+                            <Select style={{ width: 120 }} onChange={(v)=>{this.setState({staff:v})}}>
+                                {
+                                    this.props.staff.map((v, i)=> {
+                                        return (
+                                            <Option key={i} value={v}>{v.staffName}</Option>
+                                        )
+                                    })
+                                }
+                            </Select>
+                        </div>
                     </div>
+                    <div style={{margin:5}}>
+                        <h5>按渠道统计</h5>
+                        <div>
+                            <Select style={{ width: 120 }} onChange={(v)=>{this.setState({channel:v})}}>
+                                {
+                                    this.props.channel.map((v, j)=> {
+                                        return (
+                                            <Option key={i} value={v}>{v.channelName}</Option>
+                                        )
+                                    })
+                                }
+                            </Select>
+                        </div>
+                    </div>
+                    <div style={{margin:5}}>
+                        <h5>按时段统计</h5>
+                        <div>
+                            <Select style={{ width: 120 }} onChange={(v)=>{this.setState({segment:v})}}>
+                                {
+                                    this.props.segment.map((v, i)=> {
+                                        return (
+                                            <Option key={i} value={v}>{v.segmentName}</Option>
+                                        )
+                                    })
+                                }
+                            </Select>
+                        </div>
+                    </div>
+
 
                 </div>
                 <Table dataSource={this.props.customer} columns={this.state.columns} style={{marginRight:0}}/>
